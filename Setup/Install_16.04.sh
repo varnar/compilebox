ce55b69aecb3d57d23f15588bca2298066643da9
Vardan <varnar@users.noreply.github.com>
2018-03-04 18:54:51 -0800

    Update Install_16.04.sh

--- Setup/Install_16.04.sh
+++ Setup/Install_16.04.sh
@@ -4,7 +4,28 @@
 # Docker SETUP
 ###########################
 apt-get update
-apt-get install -y docker.io
+#This commented because docker.io is older version
+#apt-get install -y docker.io
+
+#Following steps to install newer version of Docker
+
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+#Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+
+#Verify that you now have the key with the fingerprint 
+#9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88, 
+#by searching for the last 8 characters of the fingerprint.
+apt-key fingerprint 0EBFCD88
+
+#Use the following command to set up the stable repository
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+apt-get install docker-ce
 
 echo "Docker Setup complete"
 
